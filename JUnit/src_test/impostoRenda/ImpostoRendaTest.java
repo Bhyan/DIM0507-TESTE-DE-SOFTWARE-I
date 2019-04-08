737b3474b247d72d690fc2619387cfeaa4e7b31b
Bryan Brito <bhyanbrito+github@gmail.com>
2019-04-08 19:49:16 -0300

    Incluido teste de exception.

--- JUnit/src_test/impostoRenda/ImpostoRendaTest.java
+++ JUnit/src_test/impostoRenda/ImpostoRendaTest.java
@@ -1,59 +1,49 @@
-package param;
+package impostoRenda;
 
-public class CalculoImpostoRenda {
+import static org.junit.Assert.*;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
-	/**
-	 * O valor de entrada deve ser um valor inteiro positivo que esta entre os seguintes intervalos:
-	 * Caso 0:
-	 * 		valor < 0
-	 * Caso 1:
-	 *		0 < valor <= 1200 -> 0% de imposto
-	 * Caso 2:
-	 *		1200 < valor <= 5000 -> 10% de imposto
-	 * Caso 3:
-	 *		5000 < valor <= 10000 -> 15% de imposto
-	 * Caso 4:
-	 *		valor > 10000 -> 20% de imposto
-	 *	
-	 * Teste 0: valor = -1
-	 * Teste 1: valor = 0, 0.1, 1199.9, 1200.0
-	 * Teste 2: valor = 1200.1, 4999.9, 5000.0
-	 * Teste 3: valor = 5000.1, 9999.9, 10000
-	 * Teste 4: valor = 10000.1, 12000
-	 * @param valor
-	 */
-	public static double calculaImposto(double valor) {
-	       //Declaração de Variáveis
-	       double imposto;
-	       double impostotot;
-	       String porcento;
+import param.CalculoImpostoRenda;
+import param.ValorMenorZero;
 
-	       if(valor >= 0) {
-	    	   if(valor <= 1200) {
-	       			imposto = 1.0;
-	       			porcento = "0%";
-	       			impostotot = 0.0;
-	       		}
-	    	   else if(valor > 1200 && valor <= 5000) {
-	    		   imposto = 1.1;
-	    		   porcento = "10%";
-	    		   impostotot = 0.1;
-	    	   }
-	    	   else if(valor > 5000 && valor <= 10000){
-	    		   imposto = 1.15;
-	    		   porcento = "15%";
-	    		   impostotot = 0.15;
-	    	   }
-	    	   else{
-	    		   imposto = 1.2;
-	    		   porcento = "20%";
-	    		   impostotot = 0.2;
-	    	   }
-	       }
-	       else {
-	    	   throw new ValorMenorZero("Fila Vazia.");
-	       }
-	     	       
-	       return valor * imposto;
-	    }
+@RunWith(Parameterized.class)
+public class ImpostoRendaTest {
+	
+	@Parameter(0)
+	public double entrada;
+	
+	@Parameter(1)
+	public double esperado;
+	
+	@Parameters
+	public static Object[][] data(){
+		return new Object[][] {
+			{0, 0.0},
+			{0.1, 0.1},
+			{1199.9, 1199.9},
+			{1200.0, 1200.0},
+			{1200.1, 1320.11},
+			{4999.9, 5499.89},
+			{5000.0, 5500.0},
+			{5000.1, 5750.115},
+			{9999.9, 11499.885},
+			{10000.0, 11500.0},
+			{10000.1, 12000.12},
+			{12000.0, 14400.0}
+		};
+	}
+
+	@Test(expected = ValorMenorZero.class)
+	public void testeValorMenorZero() {
+		CalculoImpostoRenda.calculaImposto(-1);
+	}
+
+	@Test
+	public void test() {
+		assertEquals(esperado, CalculoImpostoRenda.calculaImposto(entrada), 0.1);
+	}
 }
