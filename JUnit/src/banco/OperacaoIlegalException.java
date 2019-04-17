90d62e49bd91cc2c5f9f2422ae3759f94f6653f4
Bryan Brito <bhyanbrito+github@gmail.com>
2019-04-17 11:44:26 -0300

    Adicionado serialVersionUID

--- JUnit/src/banco/OperacaoIlegalException.java
+++ JUnit/src/banco/OperacaoIlegalException.java
@@ -2,5 +2,7 @@ package banco;
 
 public class OperacaoIlegalException extends Exception {
 
+	private static final long serialVersionUID = 1L;
+
 }
 
--- JUnit/src/param/temperature/TempException.java
+++ JUnit/src/param/temperature/TempException.java
@@ -2,6 +2,8 @@ package param.temperature;
 
 public class TempException extends Exception {
 
+	private static final long serialVersionUID = 1L;
+
 	public TempException(String string) {
 		super(string);
 	}
