19f217d1571dbb155871638179011f288127437f
Bryan Brito <bhyanbrito+github@gmail.com>
2019-04-17 11:43:50 -0300

    Testes descomentados

--- JUnit/src/basico/JUnitBasicoTest.java
+++ JUnit/src/basico/JUnitBasicoTest.java
@@ -14,41 +14,35 @@ import static org.junit.Assert.*;
 public class JUnitBasicoTest {
 
 	
-//	public void testAssertTrue() {
-//		assertTrue(false);
-//	}
+	public void testAssertTrue() {
+		assertTrue(false);
+	}
 	
 	
 	
-//	public void testAssertFalse() {
-//	assertFalse(true);
-//	}
+	public void testAssertFalse() {
+		assertFalse(true);
+	}
 
 	/*
 	 * O JUnit informa o resultado esperado e o obtido.
 	 */
+	public void testAssertEqualInts() {
+		assertEquals(1, 2);
+	}	
+		
+	public void testCheckingIntCalculation() {
+		int value = 4;
+		int doubled = value * 3;
+		assertEquals(8, doubled);
+	}	
 	
-//	public void testAssertEqualInts() {
-//		assertEquals(1, 2);
-//	}	
-	
-	
-	
-//	public void testCheckingIntCalculation() {
-//		int value = 4;
-//		int doubled = value * 3;
-//		assertEquals(8, doubled);
-//	}	
-	
-	
-
-//	public void testAssertExactlyEqualDoubles() {
-//		double value = 12.0;
-//		double result = value / 3;
-//		double delta = 0.0;
-//		assertEquals(6.0, result, delta);
-//	}
-	
+	public void testAssertExactlyEqualDoubles() {
+		double value = 12.0;
+		double result = value / 3;
+		double delta = 0.0;
+		assertEquals(6.0, result, delta);
+	}
 	
 	/*
 	 * Quando numeros flutuantes sao comparados previsamos 
@@ -57,37 +51,33 @@ public class JUnitBasicoTest {
 	 * 
 	 */
 
-//	public void testAssertAlmostEqualDoubles() {
-//		double numerator = 10.0;
-//		double denominator = 3.0;
-//		double delta = 0.00001;
-//		double expected = 3.333;
-//		double quotient = numerator/denominator;
-//		
-//		assertEquals(expected, quotient, delta);
-//	}
+	public void testAssertAlmostEqualDoubles() {
+		double numerator = 10.0;
+		double denominator = 3.0;
+		double delta = 0.00001;
+		double expected = 3.333;
+		double quotient = numerator/denominator;
+		
+		assertEquals(expected, quotient, delta);
+	}
 	
 	/*
 	 * Strings can be compared too. Make this test pass by fixing
 	 * whatToSay.  Before you fix it, note how JUnit indicates where the
 	 * expected and actual strings differ
 	 */
-	
-//	public void testCompareStrings() {
-//		String greeting = "Hello";
-//		String greetee = "World";
-//		
-//		String whatToSay = greeting + greetee;
-//		
-//		assertEquals("Hello World", whatToSay);
-//	}
-	
-	
-//	public void testCompareStrings() {
-//		int[] array1 = {1,2,3,4};
-//		int[] array2 = {5,1,2,3,4};
-//		assertArrayEquals(array1, array2);
-//	}
-	
+	public void testCompareStrings() {
+		String greeting = "Hello";
+		String greetee = "World";
 		
+		String whatToSay = greeting + greetee;
+		
+		assertEquals("Hello World", whatToSay);
+	}
+
+	public void testCompareArray() {
+		int[] array1 = {1,2,3,4};
+		int[] array2 = {1,2,3,4};
+		assertArrayEquals(array1, array2);
+	}
 }
